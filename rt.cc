db092c08b9bae8855effcdf4f25ef6306983c66b
qoala <ed.gonzalez3@gmail.com>
2012-12-11 00:04:52 -0800

    Added mapping of type string to read_* function to rt.cc:main
    There's also some test code for reading objects in there that's being saved
    before that entire section is gutted in favor of reading a script file

--- rt.cc
+++ rt.cc
@@ -8,12 +8,23 @@
 #include "plane.hh"
 #include "sphere.hh"
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
 
 using namespace std;
 using namespace boost;
 
 int main()
 {
+  // Map of type names to SceneObjectReader functions
+  map<string, SceneObjectReader> readFuncs;
+
+  // Add types to the mapping
+  readFuncs["plane"] = read_Plane;
+  readFuncs["sphere"] = read_Sphere;
+
+  // Create an empty scene
   Scene scn = Scene();
 
   // Create some objects & lights and give them to the Scene
@@ -32,6 +43,24 @@ int main()
     SPLight l2 = SPLight(new Light(Vector3F({5, 3, 5}),
                                    Color(0.3, 0.3, 0.3)));
 
+    /*
+    // Test reading planes
+    istringstream iss("0 (0 1 0) [0.5 0 0.5]");
+    scn.add_object(read_Plane(iss));
+    */
+
+    /*
+    // Test reading spheres
+    istringstream iss("(-1.2 0.5 0) 0.5 [1 0 0]");
+    scn.add_object(read_Sphere(iss));
+    */
+
+    /*
+    // Test reading lights
+    istringstream iss("(-10 10 5) [0.8 0.8 0.8]");
+    scn.add_light(read_Light(iss));
+    */
+
     scn.add_object(pln);
     scn.add_object(s1);
     scn.add_object(s2);
@@ -41,10 +70,15 @@ int main()
     scn.add_light(l2);
   }
 
+  /*
   // Create a camera to render the scene from
   Camera cam = Camera(Vector3F({-1.5, 1, 3}),   // Position
                       Vector3F({-0.3, 0.5, 0}), // Look-at target
                       Vector3F({0, 1, 0}));     // Up
+  */
+  // Test reading cameras
+  istringstream iss("(-1.5 1 3) (-.3 .5 0) (0 1 0)");
+  Camera cam = read_Camera(iss);
 
   // Render the scene to std out
   scn.render(cam, 500, cout);
